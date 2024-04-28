85bfc1389f2ee138309bd2a18834e2610bc215b9
camibuldin <buldinma@gmail.com>
2024-04-28 14:06:50 +0200

    initial commit

---
+++ src/app/favoritos/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "../page.module.css";
+
+export default function Favoritos() {
+  return (
+    <main className={styles.main}>
+   <h1>favoritos</h1>
+    </main>
+  );
+}
---
+++ src/app/productos/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "../page.module.css";
+
+export default function Productos() {
+  return (
+    <main className={styles.main}>
+   <h1>productos</h1>
+    </main>
+  );
+}
